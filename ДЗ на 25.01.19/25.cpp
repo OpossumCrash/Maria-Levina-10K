6b99ccea932856af7428507550d963086b98f417
OpossumCrash <44146018+OpossumCrash@users.noreply.github.com>
2019-02-25 18:19:15 +0300

    Update 25.cpp

--- ДЗ на 25.01.19/25.cpp
+++ ДЗ на 25.01.19/25.cpp
@@ -4,3 +4,39 @@
 меньше чем 4 (разница в индексах элементов пары должна быть 4 или более,
 пор€док элементов в паре неважен). Ќеобходимо определить количество таких
 пар, дл€ которых произведение элементов делитс€ на 38.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int chet = 0;
+
+ int N; cin >> N;
+
+ if(N <= 3){
+   cout << "None" << endl;
+   return 0;
+ };
+
+ int* mas = new int [N];
+
+ for(int i = 0; i < N; i++){
+    cin >> mas[i];
+ };
+
+ for(int i = 0; i <= (N - (4 + 1)); i++){
+
+    for(int j = 4; j <= (4 - i); j++){
+        int temp = mas[i + j] * mas[i];
+
+        if((temp % 38) == 0){
+          chet++;
+        };
+    };
+ };
+
+ cout << "The amount of pairs is " << chet;
+ return 0;
+
+ }
